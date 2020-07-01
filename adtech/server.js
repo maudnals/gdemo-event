5d4fd43e44924e0a3d432e28dd7e7f7d9ff5ff23
maudnals <maud.nalpas@gmail.com>
2020-07-01 15:23:59 +0200

    feat: bootstrap adtech server

---
+++ adtech/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.static('public'))
+
+app.get('/ad', (req, res) => {
+  res.sendFile(__dirname + '/ad.html')
+})
+
+app.get('/script', (req, res) => {
+  res.sendFile(__dirname + '/script.js')
+})
+
+const listener = app.listen(8000, () => {
+  console.log('App is listening on port ' + listener.address().port)
+})
