3ebac3b8c62847d74a9bce12e825f48a55620293
maudnals <maud.nalpas@gmail.com>
2020-07-01 15:24:20 +0200

    feat: changed static sports

--- adtech/script.js
+++ adtech/script.js
@@ -1,4 +1,4 @@
-console.log("✔️ Loaded adtech script");
+console.log('✔️ Loaded adtech script')
 document.write(
-  "<iframe src='http://127.0.0.1:8081/index-1.html' width=200 height=230 scrolling=no frameborder=1 padding=0></iframe>"
-);
+  "<iframe src='http://127.0.0.1:8000/ad' width=200 height=230 scrolling=no frameborder=1 padding=0></iframe>"
+)
