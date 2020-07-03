ac92eb610edf72f61b6d9d4458c1087b2d3c924d
maudnals <maud.nalpas@gmail.com>
2020-07-03 14:40:05 +0200

    feat: added conversion logics and redirect

--- adtech/server.js
+++ adtech/server.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 
+// Adtech conversion logics
+
+const priceBuckets = {
+  $80: '1',
+  $120: '2',
+  $200: '3'
+}
+
+const prices = Object.keys(priceBuckets)
+
+const maxValue = priceBuckets[prices[prices.length - 1]]
+
+function toHex(value, maxValue) {
+  return (parseInt(value, 16) % maxValue).toString(16)
+}
+
+const getConversionData = (value) => toHex(value, maxValue)
+
+// Server
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
@@ -16,7 +36,12 @@ app.get('/script', (req, res) => {
 })
 
 app.get('/conversion', (req, res) => {
-  console.log(req.query)
+  const priceBucket = priceBuckets[req.query.price]
+  const conversionData = getConversionData(priceBucket)
+  res.redirect(
+    302,
+    `/.well-known/register-conversion?conversion-data=${conversionData}`
+  )
 })
 
 const listener = app.listen(8000, () => {
