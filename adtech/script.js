77a3117dcd9c8560a43ee036a4fbdcdfbdefdf90
maudnals <maud.nalpas@gmail.com>
2020-07-07 23:56:08 +0200

    fix: added missing feature policy

--- adtech/script.js
+++ adtech/script.js
@@ -1,4 +1,4 @@
 console.log('✔️ Loaded adtech script')
 document.write(
-  "<iframe src='http://127.0.0.1:8000/ad' width=200 height=230 scrolling=no frameborder=1 padding=0></iframe>"
+  "<iframe src='http://127.0.0.1:8000/ad' allow='conversion-measurement' width=200 height=230 scrolling=no frameborder=1 padding=0></iframe>"
 )
