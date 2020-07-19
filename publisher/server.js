5cd1edb519da1662b71255b6af9331ea15b3f6f5
maudnals <maud.nalpas@gmail.com>
2020-07-20 00:09:02 +0200

    chore: added node server for publisher

---
+++ publisher/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const listener = app.listen(PORT, () => {
+  console.log(
+    '📰 Publisher server is listening on port ' + listener.address().port
+  )
+})
